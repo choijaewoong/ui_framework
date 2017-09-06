4ed3f73a0126df8ece14963a12c2679fe8749349
choijaewoong <jwoongchoi@smilegate.com>
2017-09-06 19:03:17 +0900

    component:button component 페이지 구현

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,18 @@ var express = require('express');
 
 module.exports = function(app) {
 
+    app.get('*', function(req, res, next) {
+        var pathname = req.params[0],
+            isHtml = pathname.substr(pathname.length - 4) === 'html';
+        if (isHtml) {
+            pathname = pathname.replace('.html', '').substr(1);
+            res.render(pathname);
+        } else {
+            next();
+        }
+    });
+
+
     app.get('/', function(req, res) {
         res.render('index.html');
     });
