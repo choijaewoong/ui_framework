a4867701c413a0209c13fe087855f510d9c2b349
choijaewoong <jwoongchoi@smilegate.com>
2017-09-06 11:24:59 +0900

    env:로컬 node server 세팅

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    ejs = require('ejs-locals');
+
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', ejs);
+app.use(express.static(__dirname + '/assets'));
+
+var router = require('./routes')(app);
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+    console.log("app listening on port " + port);
+});
