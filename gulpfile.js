ffdbeb209d67e1442dad726b4bf75ef363eb5a7c
choijaewoong <jwoongchoi@smilegate.com>
2017-09-06 14:10:09 +0900

    env:gulp 자동화 도구 세팅

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon'),
+    cleanCss = require('gulp-clean-css'),
+    concatCss = require('gulp-concat-css');
+
+var PATHS = {};
+PATHS.css = ['./assets/css/partials/_reset.css'];
+
+gulp.task('concatCss', function() {
+    return gulp.src(PATHS.css)
+        .pipe(concatCss("style.css"))
+        .pipe(cleanCss())
+        .pipe(gulp.dest("./assets/css/"));
+});
+
+gulp.task('nodemon', function() {
+    return nodemon({
+        script: './server.js',
+        ext: 'js'
+    }).on('restart', function() {
+        console.log('NODEMON RESTART :)');
+    });
+});
+
+gulp.task('default', gulp.series('concatCss', 'nodemon'));
